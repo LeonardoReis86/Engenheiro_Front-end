eca7f1ee4ce3f66ffd55846ed3cb7ad7913ae911
Leonardo Reis <leorsouza0311@gmail.com>
2023-11-10 18:23:50 -0300

    Implement logic for adding a row to the table and inserting it into the tbody

--- Calculadora_de_medias/script.js
+++ Calculadora_de_medias/script.js
@@ -7,6 +7,21 @@ form.addEventListener('submit', function(e) {
    const inputNomeAtividade = document.getElementById('atividade');
    const inputNotaAtividade = document.getElementById('nota');
    
-   alert(`Atividade: ${inputNomeAtividade.value} - Nota: ${inputNotaAtividade.value}`);
 
-})
+   //criação de linha na tabela
+   let linha = '<tr>';
+   linha += `<td>${inputNomeAtividade.value}</td>`;
+   linha += `<td>${inputNotaAtividade.value}</td>`;
+   linha += `<td>${inputNotaAtividade.value >= 7 ? 'Aprovado' : 'Reprovado'}</td>`;
+   linha += '</tr>';
+
+   //recuperar o corpo da tabela
+   const corpoTabela = document.querySelector('tbody');
+
+   // inserir um conteúdo dentro de uma tag
+   corpoTabela.innerHTML = linha;
+
+
+   
+
+});
